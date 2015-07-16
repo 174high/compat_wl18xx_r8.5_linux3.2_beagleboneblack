58ddba832c04f95b07d6833d40f53d8163422b54
root <root@snq-Precision-T1700.(none)>
2015-07-16 15:21:26 +0800

    linux3.2 beagleboneblack r8.5

---
+++ build.sh
@@ -0,0 +1,2 @@
+#/bin/sh
+make CROSS_COMPILE=arm-eabi- KLIB=/home/share/android/TI_Android_JB_4.2.2_DevKit_4.1.1/kernel KLIB_BUILD=/home/share/android/TI_Android_JB_4.2.2_DevKit_4.1.1/kernel ARCH=arm
